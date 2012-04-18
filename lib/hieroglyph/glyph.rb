83fabfcf0cd7fb75d01871011a2a74c0a0c50a51
Doug Avery <dougunderscorenelson@gmail.com>
2012-04-17 20:24:54 -0700

    Better shape feedback

--- lib/hieroglyph/glyph.rb
+++ lib/hieroglyph/glyph.rb
@@ -7,13 +7,74 @@ module Hieroglyph
 
     attr_accessor :name
 
+    SHAPE_HANDLERS = {
+      "polygon" => "convert_polygon",
+      "path" => "convert_path",
+      "circle" => "report_invalid",
+      "ellipse" => "report_invalid",
+      "line" => "report_invalid",
+      "polyline" => "report_invalid",
+      "rect" => "report_invalid"
+    }
+
+    @@too_many_shapes = false
+
     def initialize(file, source)
       @name = file.gsub(source, "").gsub("/", "").each_char.first
-      Hieroglyph.log "  #{@name} -> reading..."
       @contents = Nokogiri::XML(File.new(file))
-      @polygon = @contents.root.at_css("polygon")
-      @path = @polygon.nil? ? convert_path: convert_polygon
+      @path = Savage::Path.new
+      Hieroglyph.log "  #{@name} -> reading..."
+      parse_shapes
       flip_paths
+    end
+
+    def parse_shapes
+      count = 0
+      SHAPE_HANDLERS.each do |type, method|
+        contents = @contents.root.at_css(type)
+        if contents
+          count += 1
+          if count > 1
+            report_too_many
+          else
+            self.method(method).call(type, contents)
+          end
+        end
+      end
+    end
+
+    def convert_polygon(type, content)
+      Hieroglyph.log "       polygon found - converting"
+      points = content["points"].split(" ")
+      return_path = @path
+      Savage::Path.new do |path|
+        start_position = points.shift.split(",")
+        path.move_to(start_position[0], start_position[1])
+        points.each do |point|
+          position = point.split(",")
+          path.line_to(position[0], position[1])
+        end
+        path.close_path
+        return_path = path
+      end
+      return_path
+    end
+
+    def convert_path(type, content)
+      Hieroglyph.log "       path found"
+      Savage::Parser.parse content["d"]
+    end
+
+    def report_invalid(type, content)
+      Hieroglyph.log "       #{type} found - this shape is invalid!"
+      Hieroglyph.log "       'make compound path' in your vector tool to fix"
+    end
+
+    def report_too_many
+      unless @too_many
+        Hieroglyph.log "       too many shapes! your icon might look weird as a result"
+        @too_many = true
+      end
     end
 
     def flip_paths
@@ -54,25 +115,6 @@ module Hieroglyph
             end
           end
         end
-      end
-    end
-
-    def convert_path
-      @path = @contents.root.at_css("path")["d"]
-      @path = Savage::Parser.parse @path
-    end
-
-    def convert_polygon
-      Hieroglyph.log "    -> converting polygon to path"
-      points = @polygon["points"].split(" ")
-      Savage::Path.new do |path|
-        start_position = points.shift.split(",")
-        path.move_to(start_position[0], start_position[1])
-        points.each do |point|
-          position = point.split(",")
-          path.line_to(position[0], position[1])
-        end
-        path.close_path
       end
     end
 
